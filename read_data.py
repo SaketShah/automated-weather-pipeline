6a44c868b0503a784994d0979b2a49ec4507df13
Saket Shah <103311657+SaketShah@users.noreply.github.com>
2026-04-17 21:34:07 -0600

    Update read_data.py

--- read_data.py
+++ read_data.py
@@ -1,8 +1,13 @@
-import sqlite3
 import pandas as pd
+import os
+from sqlalchemy import create_engine
 
-# Pandas can read SQL directly into a DataFrame!
-with sqlite3.connect('weather_warehouse.db') as conn:
-    df = pd.read_sql("SELECT * FROM saskatoon_forecast", conn)
+# Make sure you set your DATABASE_URL environment variable first!
+db_url = os.environ.get("DATABASE_URL")
+engine = create_engine(db_url)
 
+# Read the cloud table directly into a Pandas DataFrame!
+df = pd.read_sql("SELECT * FROM saskatoon_forecast", engine)
+
+print("Opening the Cloud Vault...\n")
 print(df)
